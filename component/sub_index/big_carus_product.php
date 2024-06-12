11351bd284ba5b304b4b1efb4ee1e51b2d27fa06
Anar <anar.abdullazada@ufaz.az>
2024-06-12 15:43:10 +0400

    index 4tab to 2tab

---
+++ component/sub_index/big_carus_product.php
@@ -0,0 +1,3 @@
+<div class="hero__product-item">
+  <img src="assets/img/product/img_52.png" alt="">
+</div>
---
+++ component/sub_index/big_carus_product_nav.php
@@ -0,0 +1,5 @@
+<div class="hero__product-item-nav">
+  <div class="image">
+    <img src="assets/img/product/img_52.png" alt="">
+  </div>
+</div>
